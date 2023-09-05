6455602f7c514d8265bb8431644dc794d7844cab
anonymousGiga <cryptonymGong@gmail.com>
2023-09-05 11:10:19 +0800

    add

---
+++ my-lib/examples/ex1.rs
@@ -0,0 +1,6 @@
+// cargo run --example ex1
+use my_lib::add;
+fn main() {
+    let ret = add(1, 2);
+    println!("ret: {}", ret);
+}
---
+++ my-lib/examples/ex2.rs
@@ -0,0 +1,6 @@
+// cargo run --example ex2
+use my_lib::add;
+fn main() {
+    let ret = add(5, 5);
+    println!("ret: {}", ret);
+}
