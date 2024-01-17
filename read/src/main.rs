3ad71379cae6f436e440d31a7e35bfad06080415
anonymousGiga <cryptonymGong@gmail.com>
2024-01-17 09:16:34 +0800

    add

--- read/src/main.rs
+++ read/src/main.rs
@@ -2,17 +2,18 @@ use std::fs::File;
 use std::io::{self, BufRead};
 
 fn main() -> io::Result<()> {
-    pure_opcode()?;
+    // pure_opcode(5.61)?;
     // print()?;
-    // pure_cat()?;
+    pure_cat(5.61)?;
 
     Ok(())
 }
 
-fn pure_opcode() -> io::Result<()> {
+fn pure_opcode(rdt: f64) -> io::Result<()> {
 
     // 打开文件
-    let file = File::open("/home/andy/Source/work/experiment/20231218/opcode1")?;
+    // let file = File::open("/home/andy/Source/work/experiment/20231218/opcode1")?;
+    let file = File::open("/home/andy/Source/work/experiment/20240115/opcode1.txt")?;
     let reader = io::BufReader::new(file);
 
     let mut total = 0.0;
@@ -34,10 +35,11 @@ fn pure_opcode() -> io::Result<()> {
             let value7: f64 = fields[6].parse().unwrap();
             let value8: f64 = fields[7].parse().unwrap();
             let value9: f64 = fields[8].parse().unwrap();
-            let value10: &str = fields[9];
+            let value10: f64 = fields[9].parse().unwrap();
+            let value11: &str = fields[10];
 
 
-            let rdtsc = 4.664 * value2 as f64 / 1_000_000_000.0;
+            let rdtsc = rdt * value2 as f64 / 1_000_000_000.0;
             let after_v4 = if value4 > rdtsc {
                 value4 - rdtsc 
             } else {
@@ -49,16 +51,16 @@ fn pure_opcode() -> io::Result<()> {
             } 
 
             let after_v5 = after_v4 * 100.0 / total;
-            let after_v6 = if value6 > 4.664{
-                value6 - 4.664
+            let after_v6 = if value6 > rdt{
+                value6 - rdt
             } else {
                 0.0
             };
             let middle = 15;
             // 打印读取的值
             println!(
-                "{:<15}  {:>15}  {:>middle$.3}  {:>middle$.2}  {:>middle$.3}  {:>middle$.2}  {:>middle$.3}  {:>middle$.2}  {:>middle$} {:>middle$} ",
-                value1, value2, value3, after_v4, after_v5, after_v6, value7, value8, value9, value10
+                "{:<15}  {:>15}  {:>middle$.3}  {:>middle$.2}  {:>middle$.3}  {:>middle$.2}  {:>middle$.3}  {:>middle$.2}  {:>middle$} {:>middle$} {:>middle$}",
+                value1, value2, value3, after_v4, after_v5, after_v6, value7, value8, value9, value10, value11
             );
         }
     }
@@ -67,7 +69,8 @@ fn pure_opcode() -> io::Result<()> {
 
 fn print() -> io::Result<()> {
     // 打开文件
-    let file = File::open("/home/andy/Source/work/experiment/20231218/opcode_pure1")?;
+    // let file = File::open("/home/andy/Source/work/experiment/20231218/opcode_pure1")?;
+    let file = File::open("/home/andy/Source/work/experiment/20240115/order_opcode.txt")?;
     let reader = io::BufReader::new(file);
 
     // 循环读取每一行数据
@@ -87,16 +90,16 @@ fn print() -> io::Result<()> {
             let value7: f64 = fields[6].parse().unwrap();
             let value8: f64 = fields[7].parse().unwrap();
             let value9: f64 = fields[8].parse().unwrap();
-            let value10: &str = fields[9];
-            let value11: f64 = fields[10].parse().unwrap();
-
+            let value10: f64 = fields[9].parse().unwrap();
+            let value11: &str = fields[10];
+            let value12: f64 = fields[11].parse().unwrap();
 
 
             let middle = 15;
             // 打印读取的值
             println!(
-                "{:<15}  {:>15}  {:>middle$.3}  {:>middle$.2}  {:>middle$.3} {:>middle$.3} {:>middle$.2}  {:>middle$.3}  {:>middle$.2}  {:>middle$.2} {:>middle$} ",
-                value1, value2, value3, value4, value5, value11, value6, value7, value8, value9, value10
+                "{:<15}  {:>15}  {:>middle$.3}  {:>middle$.2}  {:>middle$.3} {:>middle$.3} {:>middle$.2}  {:>middle$.3}  {:>middle$.2}  {:>middle$.2} {:>middle$.2} {:>middle$}",
+                value1, value2, value3, value4, value5, value12, value6, value7, value8, value9, value10, value11
             );
         }
     }
@@ -104,9 +107,10 @@ fn print() -> io::Result<()> {
     Ok(())
 }
 
-fn pure_cat() -> io::Result<()> {
+fn pure_cat(rdt: f64) -> io::Result<()> {
     // 打开文件
-    let file = File::open("/home/andy/Source/work/experiment/20231218/cat")?;
+    // let file = File::open("/home/andy/Source/work/experiment/20231218/cat")?;
+    let file = File::open("/home/andy/Source/work/experiment/20240115/cc")?;
     let reader = io::BufReader::new(file);
 
     let total = 52346.45;
@@ -125,7 +129,7 @@ fn pure_cat() -> io::Result<()> {
             let _value5: f64 = fields[4].parse().unwrap();
             let value6: f64 = fields[5].parse().unwrap();
 
-            let rdtsc = 4.664 * value2 as f64 / 1_000_000_000.0;
+            let rdtsc = rdt * value2 as f64 / 1_000_000_000.0;
             let after_v4 = if value4 > rdtsc {
                 value4 - rdtsc 
             } else {
@@ -133,12 +137,18 @@ fn pure_cat() -> io::Result<()> {
             };
             let after_v5 = after_v4 * 100.0 / total;
 
+            let after_v6 = if value6 > rdt {
+                value6 - rdt
+            } else {
+                0.0
+            };
+
 
             let middle = 15;
             // 打印读取的值
             println!(
                 "{:<15}  {:>15}  {:>middle$.3}  {:>middle$.2}  {:>middle$.3} {:>middle$.3} ",
-                value1, value2, value3, after_v4, after_v5, value6 
+                value1, value2, value3, after_v4, after_v5, after_v6 
             );
         }
     }
